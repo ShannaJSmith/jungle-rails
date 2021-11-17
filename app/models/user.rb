0473fafbf71085de10df0b16353262b8fc9d0c67
ShannaJSmith <shanna_smith987@hotmail.com>
2021-11-17 23:12:41 +0000

    add user model validations and create spec tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+	validates :password, length: { minimum: 8 }
+	validates :password_confirmation, presence: true
+
 end
